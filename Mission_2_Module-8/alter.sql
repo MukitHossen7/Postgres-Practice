81d37d24a52673128e8368c29358edb12806cf53
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-17 12:15:19 +0600

    fix: comment out ALTER TABLE statements and correct INSERT statement values in alter.sql

--- Mission_2_Module-8/alter.sql
+++ Mission_2_Module-8/alter.sql
@@ -1,10 +1,10 @@
 -- Active: 1747415867666@@127.0.0.1@5432@medical_db
 SELECT * FROM doctor;
 -- ADD COLUMN
-ALTER TABLE doctor ADD COLUMN age INTEGER DEFAULT 18;
+-- ALTER TABLE doctor ADD COLUMN age INTEGER DEFAULT 18;
 
 -- DROP COLUMN
-ALTER TABLE doctor DROP COLUMN age;
+-- ALTER TABLE doctor DROP COLUMN age;
 
 -- INSERT data in TABLE
 INSERT INTO
@@ -17,21 +17,21 @@ INSERT INTO
         user_age
     )
 VALUES (
-        15,
-        'MimaKa',
+        16,
+        'MimaKal',
         'Akter',
-        '01721000000',
-        'mimaKa@gmail.com',
-        30.52
+        '01711000000',
+        'mimaKal@gmail.com',
+        80
     )
 
 -- Modify data type of a column
-ALTER TABLE doctor ALTER COLUMN fname TYPE VARCHAR(100)
+-- ALTER TABLE doctor ALTER COLUMN fname TYPE VARCHAR(100)
 
 -- Rename COLUMN
-ALTER TABLE doctor RENAME COLUMN uer_age TO user_age;
+-- ALTER TABLE doctor RENAME COLUMN uer_age TO user_age;
 
-ALTER TABLE doctor_info RENAME TO doctor;
+-- ALTER TABLE doctor_info RENAME TO doctor;
 
 ALTER TABLE doctor ADD CONSTRAINT user_age CHECK (user_age >= 18);
 
@@ -40,3 +40,7 @@ ALTER TABLE doctor DROP CONSTRAINT user_age;
 ALTER TABLE doctor ALTER COLUMN email SET NOT NULL;
 
 ALTER TABLE doctor ALTER COLUMN email DROP NOT NULL;
+
+ALTER TABLE doctor ADD CONSTRAINT unique_doctor_email UNIQUE (email);
+
+ALTER TABLE doctor DROP CONSTRAINT unique_doctor_email

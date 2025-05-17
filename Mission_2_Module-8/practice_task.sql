6e5fde4e34dcc3b15e8fd1faf4b8a056cff3d3c6
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-17 23:35:43 +0600

    feat: create students table and add email column with renaming

---
+++ Mission_2_Module-8/practice_task.sql
@@ -0,0 +1,19 @@
+-- Active: 1747415867666@@127.0.0.1@5432@ph
+
+-- Student table create
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    role INTEGER UNIQUE,
+    name VARCHAR(50),
+    age INTEGER,
+    department VARCHAR(50),
+    score INTEGER,
+    status VARCHAR(20),
+    last_login DATE
+)
+
+--1.Add a column email (VARCHAR) to the existing students table.
+ALTER TABLE students ADD COLUMN email VARCHAR(100)
+
+-- 2.Rename the column email to student_email
+ALTER TABLE students RENAME COLUMN email To student_email

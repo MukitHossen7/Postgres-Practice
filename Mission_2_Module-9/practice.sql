d5424956aba921a6d66a2f487b6e34818fc632bd
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-19 19:49:44 +0600

    feat: add detailed employee query with salary and department name

--- Mission_2_Module-9/practice.sql
+++ Mission_2_Module-9/practice.sql
@@ -231,3 +231,11 @@ VALUES (
 SELECT * FROM departments;
 
 SELECT * FROM employees;
+
+SELECT
+    employee_name,
+    salary,
+    hire_date,
+    department_name
+FROM employees
+    INNER JOIN departments ON employees.department_id = departments.department_id;

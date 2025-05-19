219645776af1ac58eba8608411f07e88153afead
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-19 22:35:54 +0600

    feat: insert initial data for departments and students, and add query for recent logins

--- Mission_2_Module-9/practice_tasks_module_9.sql
+++ Mission_2_Module-9/practice_tasks_module_9.sql
@@ -16,3 +16,38 @@ CREATE TABLE courses (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL
 )
+
+INSERT INTO
+    departments (name)
+VALUES ('CSE'),
+    ('EEE'),
+    ('CSE'),
+    ('EEE'),
+    ('BBA'),
+    ('CSE'),
+    ('BBA'),
+    ('CSE');
+
+INSERT INTO
+    students (
+        name,
+        department_id,
+        last_login
+    )
+VALUES ('Alice', 1, '2025-05-12'),
+    ('Bob', 2, '2024-05-23'),
+    ('Charlie', 1, '2023-07-11'),
+    ('David', 2, '2025-04-30'),
+    ('Eva', 3, '2024-03-15'),
+    ('Farhan', 1, '2023-10-30'),
+    ('Grace', 4, '2022-09-18'),
+    ('Hafsa', 1, '2022-01-01');
+
+SELECT * FROM students;
+
+-- DROP TABLE students;
+
+SELECT *
+FROM students
+WHERE
+    last_login >= CURRENT_DATE - INTERVAL '30 days'

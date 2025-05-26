8d0a8174076c9745ad23db3483f0187e1d461e8b
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-26 15:36:43 +0600

    feat: create employees table with relevant fields

---
+++ outer-practice/practice.sql
@@ -0,0 +1,10 @@
+-- Active: 1747415867666@@127.0.0.1@5432@bankdb
+CREATE TABLE employees (
+    emp_id SERIAL PRIMARY KEY,
+    fname VARCHAR(50) NOT NULL,
+    lname VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    dept VARCHAR(50) NOT NULL,
+    salary INTEGER NOT NULL,
+    hire_date DATE
+)

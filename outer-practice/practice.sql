d2038ad8b3f7f1b7ea339166a1b70b389d7c6f69
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-26 16:03:40 +0600

    fix: add additional SELECT statements for employee salary and department queries

--- outer-practice/practice.sql
+++ outer-practice/practice.sql
@@ -115,3 +115,9 @@ VALUES (
 SELECT * FROM employees;
 
 SELECT * FROM employees WHERE dept = 'HR'
+
+SELECT * FROM employees WHERE salary >= 50000;
+
+SELECT * FROM employees WHERE dept = 'HR' OR dept = 'Finance';
+
+SELECT * FROM employees WHERE dept = 'IT' AND salary > 50000;

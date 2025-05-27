bdbf54ce3efaaed623adc7af247bd67ca1b784ae
MukitHossen7 <hossenmukit7@gmail.com>
2025-05-27 12:03:35 +0600

    fix: add customer and orders data insertion and retrieval queries in practice2.sql

--- outer-practice/practice2.sql
+++ outer-practice/practice2.sql
@@ -13,3 +13,22 @@ CREATE TABLE orders (
     cust_id INT NOT NULL,
     FOREIGN KEY (cust_id) REFERENCES customer (cust_id) ON DELETE CASCADE
 )
+
+INSERT INTO
+    customer (cust_name, cust_email)
+VALUES ('Raju', 'raju@gmail.com'),
+    ('Sham', 'sham@gmail.com'),
+    ('Paul', 'paul@gmail.com'),
+    ('Alex', 'alex@gmail.com');
+
+INSERT INTO
+    orders (ord_date, cust_id, price)
+VALUES ('2024-01-01', 1, 250.00),
+    ('2024-01-15', 1, 300.00),
+    ('2024-02-01', 2, 150.00),
+    ('2024-03-01', 3, 450.00),
+    ('2024-04-04', 2, 550.00);
+
+SELECT * FROM customer;
+
+SELECT * FROM orders;

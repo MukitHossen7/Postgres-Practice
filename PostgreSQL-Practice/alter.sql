60821f00933a125f48c90a0aac8518ab1a58ce1e
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-05 11:21:21 +0600

    alter all command practice

---
+++ PostgreSQL-Practice/alter.sql
@@ -0,0 +1,25 @@
+-- Active: 1747415867666@@127.0.0.1@5432@school_db
+
+CREATE TABLE employe (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age SMALLINT CHECK (age >= 18)
+)
+
+ALTER TABLE employe RENAME to employee;
+
+ALTER TABLE employee ADD COLUMN email VARCHAR(50);
+
+ALTER TABLE employee DROP COLUMN email
+
+ALTER TABLE employee RENAME COLUMN name to user_name
+
+ALTER TABLE employee ALTER COLUMN email type VARCHAR(100);
+
+ALTER TABLE employee ALTER COLUMN email set NOT NULL;
+
+ALTER TABLE employee ADD CONSTRAINT email UNIQUE (email)
+
+ALTER TABLE employee ALTER COLUMN user_name DROP NOT NULL
+
+SELECT * FROM employee

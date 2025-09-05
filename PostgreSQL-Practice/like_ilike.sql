91371dcadadc7b4c9f88e344e57faa41a46404a5
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-05 20:21:38 +0600

    like and Ilike oparator practice is done

---
+++ PostgreSQL-Practice/like_ilike.sql
@@ -0,0 +1,9 @@
+SELECT * FROM students
+
+SELECT first_name FROM students WHERE first_name LIKE '%l'
+
+SELECT first_name FROM students WHERE first_name LIKE 'A%'
+
+SELECT * FROM students WHERE first_name ILIKE 'a%'
+
+SELECT * FROM students WHERE first_name ILIKE '%a'

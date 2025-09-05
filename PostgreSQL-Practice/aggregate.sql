96f95d91aff9dc5069d023a8715c681ef2dbd19f
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-05 22:53:30 +0600

    practice aggregate function

---
+++ PostgreSQL-Practice/aggregate.sql
@@ -0,0 +1,11 @@
+SELECT * FROM students;
+
+SELECT AVG(age) as avg_age FROM students;
+
+SELECT MAX(age) as max_age FROM students;
+
+SELECT MIN(age) as min_age FROM students;
+
+SELECT SUM(age) as sum_age FROM students
+
+SELECT COUNT(age) as total_age_row FROM students

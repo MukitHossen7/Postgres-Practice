182d62ce587ed03cb4c21ef2f0bbbfd9c6128cb5
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-05 11:33:30 +0600

    some improtant sql command practice

--- PostgreSQL-Practice/alter.sql
+++ PostgreSQL-Practice/alter.sql
@@ -6,6 +6,8 @@ CREATE TABLE employe (
     age SMALLINT CHECK (age >= 18)
 )
 
+DROP TABLE students
+
 ALTER TABLE employe RENAME to employee;
 
 ALTER TABLE employee ADD COLUMN email VARCHAR(50);
@@ -18,8 +20,17 @@ ALTER TABLE employee ALTER COLUMN email type VARCHAR(100);
 
 ALTER TABLE employee ALTER COLUMN email set NOT NULL;
 
-ALTER TABLE employee ADD CONSTRAINT email UNIQUE (email)
+ALTER TABLE employee
+ADD CONSTRAINT unique_employee_email UNIQUE (email)
 
 ALTER TABLE employee ALTER COLUMN user_name DROP NOT NULL
+
+ALTER TABLE employee ALTER COLUMN user_name SET DEFAULT 'test'
+
+ALTER TABLE employee ALTER COLUMN user_name DROP DEFAULT
+
+ALTER TABLE employee DROP CONSTRAINT email
+
+INSERT INTO employee (age, email) VALUES (20, 'min@gmail.com')
 
 SELECT * FROM employee

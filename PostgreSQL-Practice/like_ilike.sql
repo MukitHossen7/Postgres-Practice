ff52b1917cad4878f3050d57333e344bbfd4d328
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-05 21:32:46 +0600

    practice some scaller function and not oparator

--- PostgreSQL-Practice/like_ilike.sql
+++ PostgreSQL-Practice/like_ilike.sql
@@ -7,3 +7,9 @@ SELECT first_name FROM students WHERE first_name LIKE 'A%'
 SELECT * FROM students WHERE first_name ILIKE 'a%'
 
 SELECT * FROM students WHERE first_name ILIKE '%a'
+
+SELECT * FROM students WHERE NOT age > 20
+
+SELECT LENGTH(first_name) as first_name_length
+FROM students
+ORDER BY first_name_length DESC

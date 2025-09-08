6103baaf170454c87111cde9a94d267e27e14d3f
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-08 16:41:48 +0600

    groupby with having is practice is complete

--- PostgreSQL-Practice/groupBy.sql
+++ PostgreSQL-Practice/groupBy.sql
@@ -13,5 +13,28 @@ SELECT
     COUNT(*) as count_student,
     AVG(age) as avg_age
 FROM students
+WHERE
+    age >= 21
 GROUP BY
     grade;
+
+SELECT * FROM students WHERE age >= 21
+
+SELECT course, COUNT(*) as student_length
+FROM students
+GROUP BY
+    course
+HAVING
+    COUNT(*) > 4
+
+-- Countries where average student age is greater than 21
+
+SELECT
+    country,
+    COUNT(*) as student_length,
+    avg(age) as avg_age
+FROM students
+GROUP BY
+    country
+HAVING
+    avg(age) > 21

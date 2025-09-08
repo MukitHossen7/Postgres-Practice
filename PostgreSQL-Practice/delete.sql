2a60dcd6eb0cbc3eadd2474651f50c2cf17c8dd0
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-08 15:32:28 +0600

    delete practice is done

---
+++ PostgreSQL-Practice/delete.sql
@@ -0,0 +1,9 @@
+SELECT * FROM students
+
+SELECT COUNT(*) FROM students
+
+DELETE FROM students WHERE email = 'default_51@gmail.com';
+
+DELETE FROM students WHERE age >= 21 AND grade = 'C';
+
+SELECT * FROM students WHERE age >= 21 AND grade = 'C';

87aefbd64466c2bc79f200f6dab23f0e5ae1e4a3
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-11 15:17:38 +0600

    all card operation is complete use prisma

--- Start_Prisma/src/index.ts
+++ Start_Prisma/src/index.ts
@@ -9,27 +9,55 @@ async function main() {
   //     name: "mim",
   //   },
   // });
-
   // const allUsers = await prisma.user.findMany({
   //   where: {
   //     id: 4,
   //   },
   // });
   // console.log(allUsers);
-
   // const getSingleUser = await prisma.user.findUnique({
   //   where: {
   //     id: 5,
   //   },
   // });
   // console.log(getSingleUser);
-
-  const getSingleUser = await prisma.user.findUniqueOrThrow({
-    where: {
-      id: 2,
-    },
-  });
-  console.log(getSingleUser);
+  // const getSingleUser = await prisma.user.findUniqueOrThrow({
+  //   where: {
+  //     id: 2,
+  //   },
+  // });
+  // console.log(getSingleUser);
+  // const updateUserById = await prisma.user.update({
+  //   where: {
+  //     id: 1,
+  //   },
+  //   data: {
+  //     email: "hiya@gmail.com",
+  //     name: "Hiya",
+  //   },
+  // });
+  // console.log(updateUserById);
+  // const updateAllUsers = await prisma.user.updateManyAndReturn({
+  //   where: {
+  //     profile: null,
+  //   },
+  //   data: {
+  //     profile: "https://www.fakeimage.com",
+  //   },
+  // });
+  // console.log(updateAllUsers);
+  // const deleteById = await prisma.user.delete({
+  //   where: {
+  //     id: 1,
+  //   },
+  // });
+  // console.log(deleteById);
+  // const deleteAllUsers = await prisma.user.deleteMany({
+  //   where: {
+  //     id: { gt: 2 },
+  //   },
+  // });
+  // console.log(deleteAllUsers);
 }
 
 main();
